8f6b46d0c41b674467694bfa96f9adc03b65b196
yashvi-khunt <yashvi@weybee.in>
2023-09-05 18:21:03 +0530

    responsive header and some seo related changes,removed unused js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 window.addEventListener("scroll", (e) => {
-  const nav = document.querySelector(".nav-inside");
+  const nav = document.querySelector(".main-header");
   if (window.pageYOffset > 0) {
     nav.classList.add("navbar-scroll");
   } else {
     nav.classList.remove("navbar-scroll");
   }
 });
+const displayMenu = function () {
+  document.querySelector(".navbar-collapse").classList.toggle("collapse");
+};
+
+document
+  .querySelector(".navbar-toggler")
+  .addEventListener("click", displayMenu);
